ff280a7a4fb4ab57b2e12d9d725236b812040869
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2023-11-12 05:11:32 +0800

    FIX: Link formatting

--- web/scraper.py
+++ web/scraper.py
@@ -35,13 +35,17 @@ def get_base_selector(uri: str) -> Selector:
 def get_links(uri: str) -> list[str]:
     log.info("Extracting relevant links")
     selector = get_base_selector(uri)
-    return selector.xpath("//div[@class='full-article-body']/ul[2]/a[contains(., 'Deck List')]/@href").getall()
+    suffixes = selector.xpath("//div[@class='full-article-body']/ul[2]/a[contains(., 'Deck List')]/@href").getall()
+    log.debug("Suffixes: %s", ", ".join(suffixes))
+    links = [f"https://www.pokemon.com{suffix}" for suffix in suffixes]
+    log.debug("Download Links: %s", ", ".join(links))
+    return links
 
 
 # For debug use only
 if __name__ == "__main__":
     selector = get_base_selector("https://www.pokemon.com/us/play-pokemon/about/tournaments-rules-and-resources")
     target_table = selector.xpath("//div[@class='full-article-body']/ul[2]")
-    log.debug(target_table)
-    line_selector = target_table.xpath("./a[contains(., 'Deck List')]/@href")
-    log.debug(line_selector)
+    log.debug(target_table.getall())
+    line_selector = target_table.xpath(".//a[contains(., 'Deck List')]/@href")
+    log.debug(line_selector.getall())
